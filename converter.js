27d0598713c38ae810efa5cf4e6fb33d92c371ed
Nicolas Rios <n.rios.1997@gmail.com>
2023-04-21 12:29:05 -0700

    Create converter.js

---
+++ converter.js
@@ -0,0 +1,50 @@
+import React from 'react'
+
+import styles from './converter.module.css'
+
+const Converter = (props) => {
+  return (
+    <div>
+      <div className={styles['converter']}>
+        <img
+          alt="Group60116761"
+          src="/playground_assets/group60116761-do7-900h.png"
+          className={styles['group6011']}
+        />
+        <span className={styles['text']}>
+          <span>UX/UI Designer</span>
+        </span>
+        <span className={styles['text2']}>
+          <span>Based In San Francisco</span>
+        </span>
+        <img
+          alt="HeroButton316761"
+          src="/playground_assets/herobutton316761-o3pm-200h.png"
+          className={styles['hero-button31']}
+        />
+        <img
+          alt="HeroButton416761"
+          src="/playground_assets/herobutton416761-zjn-200h.png"
+          className={styles['hero-button41']}
+        />
+        <img
+          alt="Group6116761"
+          src="/playground_assets/group6116761-llw8-400w.png"
+          className={styles['group611']}
+        />
+        <img
+          alt="icons8menurounded100146761"
+          src="/playground_assets/icons8menurounded100146761-5bo-200h.png"
+          className={styles['icons8menurounded10014']}
+        />
+        <img
+          alt="Group6216763"
+          src="/playground_assets/group6216763-efny-1700w.png"
+          className={styles['group621']}
+        />
+      </div>
+    </div>
+  )
+}
+
+export default Converter
